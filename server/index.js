bd40933121f426bd12ad4bd624e7ead11e736085
Nishant Raut <nishntraut17@gmail.com>
2024-01-23 23:37:50 +0530

    routes added

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+const sendSMS = async (body) => {
+    let messageOptions = {
+        from: process.env.TWILIO_FROM_NUMBER,
+        to: '+917385487171',
+        body: "Hello from node"
+    }
+    try {
+        const message = await client.messages.create(messageOptions);
+        console.log(message);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+app.get('/', (req, res) => {
+    sendSMS();
+    res.send('Message send')
+})
+
+app.listen(5000, () => {
+    console.log("server started running");
+})
